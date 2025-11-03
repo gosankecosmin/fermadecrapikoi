673f54f8b6c0e1f5b25a0115e328c15e81050f07
gosankecosmin <crapkoi@gmail.com>
2025-11-03 13:51:41 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,8 +2,10 @@
 document.getElementById('buton').addEventListener('click', () => {
   document.getElementById('status').textContent = 'Butonul funcționează.';
 });
-// Formular demo (doar mesaj local)
+
+// Formular demo (nu trimite real)
 document.getElementById('form').addEventListener('submit', (e) => {
   e.preventDefault();
-  document.getElementById('form-status').textContent = 'Mesaj înregistrat local. Pentru trimitere reală vom adăuga un serviciu.';
+  document.getElementById('form-status').textContent =
+    'Mesaj înregistrat local. Pentru trimitere reală vom adăuga un serviciu.';
 });
